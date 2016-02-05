437a09c2ca4838feb1d2c392ad226641109cc867
Izak Burger <isburger@gmail.com>
2016-02-05 15:05:11 +0200

    Moved web app into static subdir of product.

--- src/ib/bluelantern/__init__.py
+++ src/ib/bluelantern/__init__.py
@@ -6,7 +6,6 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.add_route('home', '/')
-    import pdb; pdb.set_trace()
-    #config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('app', 'static/web/app', cache_max_age=3600)
     config.scan()
     return config.make_wsgi_app()
