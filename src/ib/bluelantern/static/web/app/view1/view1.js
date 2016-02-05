4e4670c0e93242ad922a8dead9f84c9283784458
Izak Burger <isburger@gmail.com>
2016-02-05 16:06:21 +0200

    Call stats page, update web page.

--- src/ib/bluelantern/static/web/app/view1/view1.js
+++ src/ib/bluelantern/static/web/app/view1/view1.js
@@ -9,9 +9,35 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope', function($scope) {
+.controller('View1Ctrl', ['$scope', '$timeout', '$http',
+function($scope, $timeout, $http) {
 
     $scope.levelColors = [ "#a9d70b", "#f9c802", "#ff0000" ];
     $scope.reverseColors = [ "#ff0000", "#f9c802", "#a9d70b" ];
+    var timer = undefined;
+
+    // Fake it for now.
+    $scope.max_load = 1600;
+    $scope.ac_load = 500;
+    $scope.pv_watt = 200;
+    $scope.bat_watt = 308;
+    $scope.overall_load = (100.0 * $scope.ac_load)/$scope.max_load;
+
+    (function updateloop(){
+        timer = $timeout(function(){
+            $http({
+                method: 'GET',
+                url: '/stats'
+            }).success(function(data, stat, headers, config){
+                $scope.ac_load = data.ac_load;
+                $scope.pv_watt = data.pv_watt;
+                $scope.bat_watt = data.bat_watt;
+                $scope.overall_load = (100.0 * $scope.ac_load)/$scope.max_load;
+                updateloop();
+            });
+        }, 5000);
+    })();
+
+    $scope.$on('$destroy', function(){ $timeout.cancel(timer); });
 
 }]);
