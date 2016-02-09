f6fcbea505d3e9cc41eafc1c26825613a331ea66
Izak Burger <isburger@gmail.com>
2016-02-09 23:55:08 +0200

    Small changes, error handling if backend http server down, styling changes.

--- src/ib/bluelantern/static/web/app/view1/view1.js
+++ src/ib/bluelantern/static/web/app/view1/view1.js
@@ -14,7 +14,8 @@ function($scope, $timeout, $http) {
 
     $scope.levelColors = [ "#a9d70b", "#f9c802", "#ff0000" ];
     $scope.reverseColors = [ "#ff0000", "#f9c802", "#a9d70b" ];
-    var timer = undefined;
+    var timer = undefined,
+        backoff = 1;
 
     $scope.ac_max_load = 1;
     $scope.ac_load = 0;
@@ -28,14 +29,19 @@ function($scope, $timeout, $http) {
                 method: 'GET',
                 url: '/stats'
             }).success(function(data, stat, headers, config){
+                backoff = 1;
                 $scope.ac_max_load = data.ac_max_load;
                 $scope.ac_load = data.ac_load;
                 $scope.pv_watt = data.pv_watt;
                 $scope.bat_watt = data.bat_watt;
                 $scope.overall_load = (100.0 * $scope.ac_load)/$scope.ac_max_load;
                 updateloop();
+            }).error(function(){
+                // On error, back off exponentially up to a minute
+                backoff = Math.min(backoff*2, 32);
+                updateloop();
             });
-        }, 5000);
+        }, 2000 * backoff);
     })();
 
     $scope.$on('$destroy', function(){ $timeout.cancel(timer); });
