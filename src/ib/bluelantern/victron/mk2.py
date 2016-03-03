9b9b70df1374681647658cf2693591b9fcd57902
Izak Burger <isburger@gmail.com>
2016-03-04 00:42:46 +0200

    Turn mk2 module into a pyramid addon.

--- src/ib/bluelantern/victron/mk2.py
+++ src/ib/bluelantern/victron/mk2.py
@@ -1,35 +1,52 @@
 import sys
 from time import sleep
 import paho.mqtt.client as mqtt
+from threading import Thread
 
-def main():
+def main(mqtt_host, mqtt_port, mqtt_username, mqtt_password,
+        instance, name, serial_port):
     try:
         from serial import Serial
         from ib.victron.mk2 import MK2
-        from ib.victron.scripts.options import parser, options
     except ImportError:
         print >>sys.stderr, "ib.victron package not available"
         raise
 
     # Connect to MQTT broker
-    # TODO, extend parser and add options for hostname and port
     client = mqtt.Client()
     client.loop_start()
-    client.connect('localhost', 1883, 60)
+    client.connect(mqtt_host, mqtt_port, 60)
+    if mqtt_username is not None:
+        client.username_pw_set(mqtt_username, mqtt_password)
 
-    port = Serial(options.port, options.baudrate, timeout=options.timeout)
+    port = Serial(serial_port, 2400, timeout=1)
     mk2 = MK2(port).start()
-    # TODO, extend options to configure instance and equipment name
     try:
         while True:
             dc_info = mk2.dc_info()
             w = dc_info.ubat * dc_info.ibat
-            client.publish('battery01/inverter/power', str(w), 0)
-            client.publish('battery01/inverter/voltage', str(dc_info.ubat), 0)
-            client.publish('battery01/inverter/ampere', str(dc_info.ibat), 0)
+            client.publish('{}/{}/power'.format(instance, name), str(w), 0)
+            client.publish('{}/{}/voltage'.format(instance, name), str(dc_info.ubat), 0)
+            client.publish('{}/{}/ampere'.format(instance, name), str(dc_info.ibat), 0)
             sleep(2)
     except KeyboardInterrupt:
         pass
 
+def includeme(config):
+    mqtt_host = config.registry.settings.get('mqtt.host')
+    mqtt_port = int(config.registry.settings.get('mqtt.port', 1883))
+    mqtt_username = config.registry.settings.get('mqtt.username')
+    mqtt_password = config.registry.settings.get('mqtt.password')
+
+    serial_port = config.registry.settings.get('mk2.port')
+    instance = config.registry.settings.get('mk2.instance')
+    name = config.registry.settings.get('mk2.name')
+
+    target = lambda: main(mqtt_host, mqtt_port, mqtt_username, mqtt_password,
+        instance, name, serial_port)
+    thread = Thread(target = target)
+    thread.daemon = True
+    thread.start()
+ 
 if __name__ == "__main__":
-    main()
+    main('localhost', 1883, None, None, 'battery01', 'inverter', '/dev/ttyUSB0')
