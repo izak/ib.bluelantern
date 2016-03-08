20f7c844429252bb0a3ff6840736af6ee123398e
Izak Burger <isburger@gmail.com>
2016-03-08 23:23:09 +0200

    Update to include timestamp

--- scripts/simulate_mppt.py
+++ scripts/simulate_mppt.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import time, sleep
 from random import randint
 import paho.mqtt.client as mqtt
 
@@ -9,14 +9,15 @@ def main():
     client.connect('localhost', 1883, 60)
     try:
         while True:
+            now = int(time())
             pv_power = 600 + randint(-30, 35)
             bat_temp = 25 + randint(-1, 1)
             bat_volt = (245 + randint(-1, 1))/10.0
 
-            client.publish('battery01/mppt/power', str(pv_power), 0)
-            client.publish('battery01/mppt/voltage', str(bat_volt), 0)
-            client.publish('battery01/mppt/ampere', str(pv_power/bat_volt), 0)
-            client.publish('battery01/mppt/temperature', str(bat_temp), 0)
+            client.publish('battery01/mppt/power', "{} {}".format(now, pv_power), 0)
+            client.publish('battery01/mppt/voltage', "{} {}".format(now, bat_volt), 0)
+            client.publish('battery01/mppt/ampere', "{} {}".format(now, pv_power/bat_volt), 0)
+            client.publish('battery01/mppt/temperature', "{} {}".format(now, bat_temp), 0)
 
             sleep(1)
     except KeyboardInterrupt:
