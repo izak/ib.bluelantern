36219bef4dcc12f74e7f68b3fc4095b9e53f1375
Izak Burger <isburger@gmail.com>
2016-03-08 23:25:05 +0200

    Include timestamp in simulated load.

--- scripts/simulate_load.py
+++ scripts/simulate_load.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import time, sleep
 from random import randint
 import paho.mqtt.client as mqtt
 
@@ -9,13 +9,14 @@ def main():
     client.connect('localhost', 1883, 60)
     try:
         while True:
+            now = int(time())
             ac_load = 500 + randint(-25, 25)
             bat_temp = 25 + randint(-1, 1)
             bat_volt = (245 + randint(-1, 1))/10.0
 
-            client.publish('battery01/inverter/power', str(ac_load), 0)
-            client.publish('battery01/inverter/voltage', '230', 0)
-            client.publish('battery01/inverter/ampere', str(ac_load/230.0), 0)
+            client.publish('battery01/inverter/power', "{} {}".format(now, ac_load), 0)
+            client.publish('battery01/inverter/voltage', '{} 230'.format(now), 0)
+            client.publish('battery01/inverter/ampere', '{} {}'.format(now, ac_load/230.0), 0)
 
             sleep(1)
     except KeyboardInterrupt:
