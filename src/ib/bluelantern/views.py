69cca8c34f0bb9b74742ad10ba93928524eb8600
Izak Burger <isburger@gmail.com>
2016-03-19 10:56:27 +0200

    Add support for monitor devices.
    If you have a monitor device in your equipment configuration, it will use
    that figure for the battery instead of calculating it as the difference
    between PV and load. Instead, it then calculates PV as the sum of load and
    battery charging.

--- src/ib/bluelantern/views.py
+++ src/ib/bluelantern/views.py
@@ -11,15 +11,36 @@ def stats(request):
     cache = request.registry.getUtility(IEquipmentCache)
     ac_load = ac_max_load = pv_watt = pv_max_watt = bat_watt = 0.0
     for instance, a in cache.items():
+        has_bmv = False # Assume no BMV until you find one
+        instance_pv = instance_load = instance_bat = 0
+
         for id, b in a.items():
             t = b.get('type', None)
             if t == 'pv':
-                pv_watt += float(b.get('power', 0))
+                instance_pv += float(b.get('power', 0))
                 pv_max_watt += float(b.get('max_power', 0))
             elif t == 'load':
-                ac_load += float(b.get('power', 0))
+                instance_load += float(b.get('power', 0))
                 ac_max_load += float(b.get('max_power', 0))
-    bat_watt = pv_watt - ac_load
+            elif t == 'monitor':
+                has_bmv = True
+                instance_bat += float(b.get('power', 0))
+
+        if has_bmv:
+            # Estimate PV watts as load plus whatever makes it into the
+            # battery. This uses the more accurate BMV number and takes
+            # inefficiencies from the PV figure.
+            ac_load += instance_load
+            pv_watt += (instance_load + instance_bat)
+        else:
+            # No BMV. Estimate battery delta as difference between
+            # charge and load
+            ac_load += instance_load
+            pv_watt += instance_pv
+            instance_bat += (instance_pv - instance_load)
+
+        bat_watt += instance_bat
+
     return {
         'ac_load': ac_load,
         'ac_max_load': ac_max_load,
